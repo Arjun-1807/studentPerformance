825f987cc00cf97978dc4904adcc4202a75a6fbc
Arjun Gowda <arjun2005gowda@gmail.com>
2025-10-02 14:17:42 +0530

    fixed errors in logger and exception

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys
+import logging
 
 def error_message_details(error, error_detail:sys):
     _, _, exc_tb = error_detail.exc_info()
@@ -6,14 +7,16 @@ def error_message_details(error, error_detail:sys):
     file_name = exc_tb.tb_frame.f_code.co_filename
     line_no = exc_tb.tb_lineno
     
-    error_message = "Error occurred in Python script name [{0}] line number [{1}] error message [{2}]".format(file_name, line_no,str(error) )
+    error_message = "Error occurred in Python script name [{0}] line number [{1}] error message [{2}]".format(file_name, line_no,str(error))
     
     return error_message
     
 class CustomException(Exception):
     def __init__(self, error_message, error_detail:sys):
-        super.__init__(error_message)
+        super().__init__(error_message)
         self.error_message = error_message_details(error_message, error_detail=error_detail)
         
     def __str__(self):
         return self.error_message
+    
+    
